835bf3eed1362d650068a0277aa4856f0c650690
patli1998 <pratikpurohit1998@gmail.com>
2023-01-17 18:35:26 -0500

    fixing code before final

--- Pybank/main.py
+++ Pybank/main.py
@@ -3,52 +3,24 @@ import os
 import csv
 
 ## path ##
-budget_data = "./Pybank/Resources/budget_data.csv"
+budget_file = "./Challenge3-python/PyBank/Resources/budget_data.csv"
+print('Get current working directory : ', os.getcwd())
 
-with open(budget_data) as csvfile:
-    reader = csv.DictReader(csvfile)
 
-## loop stored in Dict ##
+total_number_of_months = 0
+total = 0
+with open(budget_file, newline='') as csvfile:
+    spamreader = csv.DictReader(csvfile, delimiter=',')
+    ## next(csvfile) ##
+    for row in spamreader:
+       
+        total_number_of_months+=1
+        total+=int(row["Profit/Losses"]) 
 
-for row in reader:
-    budget_data.append({"month": row["date"], "amount": int(row["Profit/Losses"]),"change": 0})
+average = total / total_number_of_months
 
-## total ##
-
-months_total=len(budget_data)
-
-## Change in Months ##
-
-Old_Amount=budget_data[0]["amount"]
-for i in range(months_total):
-    budget_data[i]["change"]=budget_data[i]["amount"] - Old_Amount
-    Old_Amount = budget_data[i]["amount"]
-
-## Finding total Amount ##
-total_amount=sum(row['amount']for row in budget_data)
-
-## total change and avg of changes ##
-
-total_change=sum(row['change']for row in budget_data)
-Average=round(total_change / (months_total-1),2)
-
-## greatest increase and decrease ##
-greatest_increase = {"month": "", "change": 0}
-greatest_decrease = {"month": "", "change": 0}
-
-if row["change"] > greatest_increase["change"]:
-                    greatest_increase["month"] = row["month"]
-                    greatest_increase["change"] = row["change"]
-elif row["change"] < greatest_decrease["change"]:
-                    greatest_decrease["month"] = row["month"]
-                    greatest_decrease["change"] = row["change"]
-
-## print all ##
-
-print("Financial_Analysis")
-print("---------------------------------")
-print(f"Total Months: {months_total}")
-print(f"Total: ${total_amount}")
-print(f"Average Change: ${round(Average,2)}")
-print(f"Greatest Increase in Profits: {greatest_increase['month']} (${greatest_increase['change']})")
-print(f"Greatest Decrease in Profits: {greatest_decrease['month']} (${greatest_decrease['change']})")
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_number_of_months}")
+print(f"Total: ${total}")
+print(f"Average Change: ${average}")
