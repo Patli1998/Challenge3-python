22a9c9d0e2575f36b9a5b7007778313486424d34
patli1998 <pratikpurohit1998@gmail.com>
2023-01-17 23:31:25 -0500

    submit: Final Submission

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,4 +3,48 @@ import os
 import csv
 
 #path
-budget
+election_file = "./PyPoll/Resources/election_data.csv"
+total_number_of_votes_cast = 0
+votes = dict()
+with open(election_file, newline='') as csvfile:
+    spamreader = csv.DictReader(csvfile, delimiter=',')
+    ## next(csvfile) ##
+    for row in spamreader:
+        total_number_of_votes_cast += 1
+        if row["Candidate"] in votes:
+            votes[row["Candidate"]] += 1
+        else:
+            votes[row["Candidate"]] = 1
+
+winner = max(votes, key=votes.get)
+
+
+
+
+
+
+print(f"Election Results")
+print(f"-------------------------")
+print(f"Total Votes: {total_number_of_votes_cast}")
+print(f"-------------------------")
+for candidate in votes.keys():
+    print(f"{candidate} {round(100*(votes[candidate]/ total_number_of_votes_cast), 3)}% ({votes[candidate]})")
+print(f"-------------------------")
+print(f"Winner: {winner}")
+
+print(f"-------------------------")
+
+
+
+with open('./PyPoll/Analysis/output.txt', 'w') as f:
+    f.write(f"Election Results\n")
+    f.write(f"-------------------------\n")
+    f.write(f"Total Votes: {total_number_of_votes_cast}\n")
+    f.write(f"-------------------------\n")
+    for candidate in votes.keys():
+        f.write(f"{candidate} {round(100*(votes[candidate]/ total_number_of_votes_cast), 3)}% ({votes[candidate]})\n")
+    f.write(f"-------------------------\n")
+    f.write(f"Winner: {winner}\n")
+
+    f.write(f"-------------------------\n")
+
--- Pybank/main.py
+++ Pybank/main.py
@@ -3,24 +3,69 @@ import os
 import csv
 
 ## path ##
-budget_file = "./Challenge3-python/PyBank/Resources/budget_data.csv"
-print('Get current working directory : ', os.getcwd())
+budget_file = "./PyBank/Resources/budget_data.csv"
 
-
+## calculating total months
 total_number_of_months = 0
 total = 0
+previous_amount = 0
+changes_in_profit_losses = 0
+max_profit = float('-inf')
+max_profit_months = "NA"
+min_profit = float('inf')
+min_profit_months = "NA"
 with open(budget_file, newline='') as csvfile:
     spamreader = csv.DictReader(csvfile, delimiter=',')
     ## next(csvfile) ##
     for row in spamreader:
        
         total_number_of_months+=1
+
+        # calculating totals
         total+=int(row["Profit/Losses"]) 
 
-average = total / total_number_of_months
+        change_in_profit_losses = int(row["Profit/Losses"]) - previous_amount
+        changes_in_profit_losses += change_in_profit_losses
+        print(f"Current Profit Losses {previous_amount} - {row['Profit/Losses']} = {change_in_profit_losses}")
+        # print(f"Change In Profit Losses {change_in_profit_losses}")
+        # print(f"changes in profit losses{changes_in_profit_losses}")
+
+        # Calculting greatest increase in profits
+        previous_amount = int(row["Profit/Losses"])
+        if change_in_profit_losses > max_profit:
+            max_profit = change_in_profit_losses
+            max_profit_months = row["Date"]
+
+        # Calculating greatest decrease in profits
+        if change_in_profit_losses < min_profit:
+            min_profit = change_in_profit_losses
+            min_profit_months = row["Date"]
+
+
+
+
+changes_in_profit_losses_total = change_in_profit_losses / (total_number_of_months - 1)
+
+
+
+
+
+
 
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_number_of_months}")
 print(f"Total: ${total}")
-print(f"Average Change: ${average}")
+print(f"Average Change: ${changes_in_profit_losses_total}")
+print(f"Greatest Increase in Profits: {max_profit_months} ${max_profit}")
+print(f"Greatest Decrease in Profits: {min_profit_months} ${min_profit}")
+
+
+with open('./PyBank/Analysis/output.txt', 'w') as f:
+    f.write("Financial Analysis\n")
+    f.write("----------------------------\n")
+    f.write(f"Total Months: {total_number_of_months}\n")
+    f.write(f"Total: ${total}\n")
+    f.write(f"Average Change: ${changes_in_profit_losses_total}\n")
+    f.write(f"Greatest Increase in Profits: {max_profit_months} ${max_profit}\n")
+    f.write(f"Greatest Decrease in Profits: {min_profit_months} ${min_profit}\n")
